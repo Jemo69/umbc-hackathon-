939c73c5fda6b68143dfc92c58311172ed90ac8f
Jemo69 <jemolife69@gmail.com>
2025-09-28 06:13:14 -0400

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,11 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+"rules": {
+    "react/no-unescaped-entities": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off"
+  }
 ];
 
 export default eslintConfig;
